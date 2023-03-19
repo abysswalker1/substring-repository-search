23bcdafcc643421936d2769a164691757e291254
Vladislav Filippov <vladislav.filippov.01@mail.ru>
2023-03-19 22:32:04 +0500

    UI-improvements

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ const form = document.forms[0];
 const results = document.querySelector('.results');
 
 const resultsListFromData = (data) => {
-  console.log(data)
   let inner = data.reduce((prev, item) => (
      prev +=
     `
     <div class='result-item'>
       <div class="result-item__avatar-wrap">
-        <img src=${item.owner.avatar_url} alt="" class="result-item__avatar">
+        <img src=${item.owner.avatar_url} class="result-item__avatar">
       </div>
       <div class="result-item__info">
         <h3 class="result-item__name">${item.name}</h3>
@@ -18,13 +17,15 @@ const resultsListFromData = (data) => {
     `
   ), '');
 
+  if( !inner ) {
+    return 'Ничего не найдено';
+  }
   return inner;
 }
 
 form.search.onchange = () => {
   form.classList.remove('is-empty');
 }
-
 
 form.onsubmit = (e) => {
   e.preventDefault();
@@ -36,12 +37,12 @@ form.onsubmit = (e) => {
 
   } else {
     form.classList.remove('is-empty');
+    results.innerHTML = '<h2>Loading</h2>';
   }
   
-  fetch(`https://api.github.com/search/repositories?q=${searchSubstring}+language:javascript?per_page=10`)
+  fetch(`https://api.github.com/search/repositories?q=${searchSubstring}+language:javascript&per_page=10`)
   .then(response => response.json())
   .then(data => {
     results.innerHTML = (resultsListFromData(data.items));
   })
-  .catch(error => console.error(error)); 
 }
